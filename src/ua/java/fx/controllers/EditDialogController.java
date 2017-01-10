c87d321539ac31a89f8c3acaa38deb06640c5497
Ivan Tymchenko <tymchenkolviv@gmail.com>
2017-01-10 15:43:53 +0200

    Main window and edit contact window are created

---
+++ src/ua/java/fx/controllers/EditDialogController.java
@@ -0,0 +1,20 @@
+package ua.java.fx.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class EditDialogController {
+
+    @FXML
+    private TextField txtPhone;
+
+    @FXML
+    private Button btnOk;
+
+    @FXML
+    private TextField txtNameSoname;
+
+    @FXML
+    private Button btnCancel;
+}
