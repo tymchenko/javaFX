b1b8df60b1546e0bae9f6e86d85d1b849115ffc5
Ivan Tymchenko <tymchenkolviv@gmail.com>
2017-01-10 15:54:32 +0200

    Person class is creted. .gitignore file is crested

---
+++ src/ua/java/fx/objects/Person.java
@@ -0,0 +1,25 @@
+package ua.java.fx.objects;
+
+
+public class Person {
+
+    private String nameSorname;
+    private String phone;
+
+    public String getNameSorname() {
+        return nameSorname;
+    }
+
+    public void setNameSorname(String nameSorname) {
+        this.nameSorname = nameSorname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
